3aa25cac000c29ada1cf8c586fc44eb5ae92a04d
Aziz Bekkine <abekkine@gmail.com>
2018-03-24 19:18:43 +0300

    Basic device bus implemented, along with time measurements.

--- BasicDeviceBus.h
+++ BasicDeviceBus.h
@@ -3,13 +3,44 @@
 
 #include "DeviceBusInterface.h"
 
+#include <unordered_map>
+#include <vector>
+
 class BasicDeviceBus : public DeviceBusInterface {
 public:
     BasicDeviceBus() {}
     ~BasicDeviceBus() {}
-    bool CheckChannel(uint32_t channel) { return false; }
-    void Publish(uint32_t channel, BusDataInterface * data) {}
-    void Subscribe(uint32_t channel, std::function<void(BusDataInterface *)> handler) {}
+    bool CheckChannel(uint32_t channel) {
+        auto found = subscribers_.find(channel);
+        if (found != subscribers_.end()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    void Publish(uint32_t channel, BusDataInterface * data) {
+
+        if (CheckChannel(channel)) {
+            for (auto handler : subscribers_[channel]) {
+
+                handler(data);
+            }
+        }
+    }
+    void Subscribe(uint32_t channel, std::function<void(BusDataInterface *)> handler) {
+   
+        if (false == CheckChannel(channel)) {
+
+            std::vector<std::function<void(BusDataInterface *)>> v;
+            subscribers_[channel] = v;
+        }
+
+        subscribers_[channel].push_back(handler);
+    }
+
+private:
+    std::unordered_map<uint32_t, std::vector<std::function<void(BusDataInterface *)>>> subscribers_;
 };
 
 #endif // BASIC_DEVICE_BUS_H_
--- main.cpp
+++ main.cpp
@@ -1,35 +1,54 @@
 #include "DeviceBusFactory.h"
 #include "TestScenario.h"
 
+#include <omp.h>
+
 int main() {
 
     TestScenario * test = new TestScenario(100000);
     DeviceBusInterface * bus = 0;
 
+    double start = omp_get_wtime();
+    test->DumpExpectedResults();
+    printf("Reference : %.5f\n", omp_get_wtime() - start);
+    puts("-----");
+
     // Basic device bus tests.
     bus = DeviceBusFactory::getBasicDeviceBus();
     test->RegisterDeviceBus(bus);
+    start = omp_get_wtime();
     test->Perform();
+    printf("BasicDeviceBus : %.5f\n", omp_get_wtime() - start);
     delete bus;
     
+    puts("-----");
     // ZeroMQ device bus tests.
     bus = DeviceBusFactory::getZeroMQDeviceBus();
     test->RegisterDeviceBus(bus);
+    start = omp_get_wtime();
     test->Perform();
+    printf("ZeroMQDeviceBus : %.5f\n", omp_get_wtime() - start);
     delete bus;
 
+    puts("-----");
     // RabbitMQ device bus tests.
     bus = DeviceBusFactory::getRabbitMQDeviceBus();
     test->RegisterDeviceBus(bus);
+    start = omp_get_wtime();
     test->Perform();
+    printf("RabbitMQDeviceBus : %.5f\n", omp_get_wtime() - start);
     delete bus;
 
+    puts("-----");
     // PubNub device bus tests.
     bus = DeviceBusFactory::getPubNubDeviceBus();
     test->RegisterDeviceBus(bus);
+    start = omp_get_wtime();
     test->Perform();
+    printf("PubNubDeviceBus : %.5f\n", omp_get_wtime() - start);
     delete bus;
 
+    puts("-----");
     return 0;
 }
 
